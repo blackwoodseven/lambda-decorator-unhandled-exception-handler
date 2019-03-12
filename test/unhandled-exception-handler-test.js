99ab01e501af3a3a070d84121c00a0db4508a575
Jean-Yves Moyen <jym@blackwoodseven.com>
2019-03-12 10:51:37 +0100

    Refactor tests

--- test/unhandled-exception-handler-test.js
+++ test/unhandled-exception-handler-test.js
@@ -20,6 +20,7 @@ describe('Unhandled exception handler decorator', function() {
   afterEach(() => {
     decorator.__set__('awsLogger', originalAwsLogger)
   });
+
   /*
    * prepare invocation fake context
    */
@@ -44,63 +45,104 @@ describe('Unhandled exception handler decorator', function() {
      }
    });
 
-  context('when decorated handler explodes with an unhandled exception', () => {
-    const verifyHandlerErrorsLoggedAndHidden = (handlerToDecorate, done) => {
-      const verifyErrorsAreHiddenAndLogged = (err, success) => {
-        expect(err).to.equal(decorator.DEFAULT_ERROR_MESSAGE);
-        expect(fakeLogger.args).not.to.be.undefined;
-        done()
-      };
-
-      executeHandler(handlerToDecorate, verifyErrorsAreHiddenAndLogged)
+  /*
+   * helper functions to check whether error are hidden or not
+   */
+  const verifyHandlerErrorsLoggedAndHidden = (handlerToDecorate, done) => {
+    const verifyErrorsAreHiddenAndLogged = (err, success) => {
+      expect(err).to.equal(decorator.DEFAULT_ERROR_MESSAGE);
+      expect(fakeLogger.args).not.to.be.undefined;
+      done()
     };
 
+    executeHandler(handlerToDecorate, verifyErrorsAreHiddenAndLogged)
+  };
+
+  const verifyHandlerErrorIsPassedThroughUnlogged = (handlerToDecorate, expectedError, done) => {
+    const verifyErrorHasBeenPassedThrough = (err, success) => {
+      expect(err).to.equal(expectedError);
+      expect(fakeLogger.args).to.be.undefined;
+      done()
+    };
+
+    executeHandler(handlerToDecorate, verifyErrorHasBeenPassedThrough)
+  };
+
+  const verifyDecoratorHidesError = (error, msg) => {
+    it(`should hide callback ${msg} and log them`, (done) => {
+      const handlerThatRejects_cbStyle = (event,ctx,cb) => { cb(error) };
+      verifyHandlerErrorsLoggedAndHidden(handlerThatRejects_cbStyle, done)
+    });
+
+    it(`should hide context.fail ${msg} and log them`, (done) => {
+      const handlerThatRejects_ctxFailStyle = (event,ctx,cb) => { ctx.fail(error) };
+      verifyHandlerErrorsLoggedAndHidden(handlerThatRejects_ctxFailStyle, done)
+    });
+
+    it(`should hide context.done ${msg} and log them`, (done) => {
+      const handlerThatRejects_ctxDoneStyle = (event,ctx,cb) => { ctx.done(error) };
+      verifyHandlerErrorsLoggedAndHidden(handlerThatRejects_ctxDoneStyle, done)
+    })
+  };
+
+  const verifyDecoratorPassThroughError = (error, msg) => {
+    it(`should pass callback  ${msg} without doing anything`, (done) => {
+      const handlerThatRejects_CbStyle = (event,ctx,cb) => { cb(error) };
+      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejects_CbStyle, error, done)
+    });
+
+    it(`should pass context.fail ${msg} without doing anything`, (done) => {
+      const handlerThatRejects_CtxFailStyle = (event,ctx,cb) => { ctx.fail(error) };
+      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejects_CtxFailStyle, error, done)
+    });
+
+    it(`should pass context.done ${msg} without doing anything`, (done) => {
+      const handlerThatRejects_CtxDoneStyle = (event,ctx,cb) => { ctx.done(error) };
+      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejects_CtxDoneStyle, error, done)
+    })
+  };
+
+  /*
+   * Hiding actual Errors
+   */
+  context('when decorated handler explodes with an unhandled exception', () => {
     it('should hide thrown errors and log them', (done) => {
       const handlerThatThrows = (event,ctx,cb) => { throw new Error('some unhandled error') };
       verifyHandlerErrorsLoggedAndHidden(handlerThatThrows, done)
     });
 
-    it('should hide callback non-string errors and log them', (done) => {
-      const handlerThatRejectsWithAnError_cbStyle = (event,ctx,cb) => { cb(new Error('some unhandled error')) };
-      verifyHandlerErrorsLoggedAndHidden(handlerThatRejectsWithAnError_cbStyle, done)
-    });
-
-    it('should hide context.fail non-string errors and log them', (done) => {
-      const handlerThatRejectsWithAnError_ctxFailStyle = (event,ctx,cb) => { ctx.fail(new Error('some unhandled error')) };
-      verifyHandlerErrorsLoggedAndHidden(handlerThatRejectsWithAnError_ctxFailStyle, done)
-    });
-
-    it('should hide context.done non-string errors and log them', (done) => {
-      const handlerThatRejectsWithAnError_ctxDoneStyle = (event,ctx,cb) => { ctx.done(new Error('some unhandled error')) };
-      verifyHandlerErrorsLoggedAndHidden(handlerThatRejectsWithAnError_ctxDoneStyle, done)
-    })
+    verifyDecoratorHidesError(new Error('some unhandled error'), "exceptions");
   });
 
   context('when decorated handler rejects with custom error messages', () => {
-    const SomeCustomErrorMsg = decorator.DEFAULT_ERROR_MESSAGE + "my custom message!";
-    const verifyHandlerErrorIsPassedThroughUnlogged = (handlerToDecorate, expectedError, done) => {
-      const verifyErrorHasBeenPassedThrough = (err, success) => {
-        expect(err).to.equal(expectedError);
-        expect(fakeLogger.args).to.be.undefined;
-        done()
-      };
+    verifyDecoratorPassThroughError(decorator.DEFAULT_ERROR_MESSAGE + "my custom message!", "string errors")
+  });
 
-      executeHandler(handlerToDecorate, verifyErrorHasBeenPassedThrough)
-    };
-
-    it('should pass callback string errors without doing anything', (done) => {
-      const handlerThatRejectsWithCustomerErrorMessage_CbStyle = (event,ctx,cb) => { cb(SomeCustomErrorMsg) };
-      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CbStyle, SomeCustomErrorMsg, done)
-    });
-
-    it('should pass context.fail string errors without doing anything', (done) => {
-      const handlerThatRejectsWithCustomerErrorMessage_CtxFailStyle = (event,ctx,cb) => { ctx.fail(SomeCustomErrorMsg) };
-      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CtxFailStyle, SomeCustomErrorMsg, done)
-    });
-
-    it('should pass context.done string errors without doing anything', (done) => {
-      const handlerThatRejectsWithCustomerErrorMessage_CtxDoneStyle = (event,ctx,cb) => { ctx.done(SomeCustomErrorMsg) };
-      verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CtxDoneStyle, SomeCustomErrorMsg, done)
-    })
-  })
+  // context('when decorated handler rejects with custom error object', () => {
+  //   const SomeCustomErrorObject = { errorMsg: decorator.DEFAULT_ERROR_MESSAGE + "my custom message!", customError: true };
+  //   const verifyHandlerErrorIsPassedThroughUnlogged = (handlerToDecorate, expectedError, done) => {
+  //     const verifyErrorHasBeenPassedThrough = (err, success) => {
+  //       expect(err).to.equal(expectedError);
+  //       expect(fakeLogger.args).to.be.undefined;
+  //       done()
+  //     };
+  //
+  //     executeHandler(handlerToDecorate, verifyErrorHasBeenPassedThrough)
+  //   };
+  //
+  //   it('should pass callback string errors without doing anything', (done) => {
+  //     const handlerThatRejectsWithCustomerErrorMessage_CbStyle = (event,ctx,cb) => { cb(SomeCustomErrorMsg) };
+  //     verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CbStyle, SomeCustomErrorMsg, done)
+  //   });
+  //
+  //   it('should pass context.fail string errors without doing anything', (done) => {
+  //     const handlerThatRejectsWithCustomerErrorMessage_CtxFailStyle = (event,ctx,cb) => { ctx.fail(SomeCustomErrorMsg) };
+  //     verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CtxFailStyle, SomeCustomErrorMsg, done)
+  //   });
+  //
+  //   it('should pass context.done string errors without doing anything', (done) => {
+  //     const handlerThatRejectsWithCustomerErrorMessage_CtxDoneStyle = (event,ctx,cb) => { ctx.done(SomeCustomErrorMsg) };
+  //     verifyHandlerErrorIsPassedThroughUnlogged(handlerThatRejectsWithCustomerErrorMessage_CtxDoneStyle, SomeCustomErrorMsg, done)
+  //   })
+  // });
 });
